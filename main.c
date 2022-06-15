a5af3f69b5016ad2fc9ef9df61c0cc60f56d4a55
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-06-15 11:22:57 -0400

    Changed main file to use DynIntArr function pointers

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib/structs/dynarray.h"
+#include "lib/types/string.h"
 
 int main() {
 	DynIntArr myarr = newintarr();
-	iarrpush(&myarr, 3);
-	iarrpush(&myarr, 4);
-	iarrpush(&myarr, 5);
+	myarr.push(&myarr, 3);
+	myarr.push(&myarr, 4);
+	myarr.push(&myarr, 5);
 
-	printf("%d\n", *darridx(&myarr, 0));
-	printf("%d\n", *darridx(&myarr, 1));
-	printf("%d\n", *darridx(&myarr, 2));
-	printf("%d\n", *darridx(&myarr, 3));
+	printf("%d\n", myarr.idx(&myarr, 0));
+	printf("%d\n", myarr.idx(&myarr, 1));
+	printf("%d\n", myarr.idx(&myarr, 2));
+	printf("%d\n", myarr.idx(&myarr, 3));
 	return 0;
 }
